834f75fe715eafe599647c1230ae0b881ac2d734
KatzeWire <jscalett@ucsc.edu>
2017-01-19 11:43:09 -0800

    Added Base cube with health
    Test calling health depletion function next

---
+++ Assets/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public float health = 100f; // how much health the base has
+
+    // Damage taken depending on amount
+    public void RemoveHealth(float amount) {
+        health -= amount;
+        // Destroy base if health is depleted
+        if (health <= 0){
+            Destroy(gameObject);
+        }
+    }
+}
