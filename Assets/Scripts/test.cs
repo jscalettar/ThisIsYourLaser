395f2203f5de7b1c97e4654fc545a3c748eea432
Andy Espejo <Andy Espejo>
2017-03-01 03:46:30 -0800

    Added more test stuff

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -16,28 +16,44 @@ public class test : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        //Test 1
+        //Test 1 - 
         test1.Add( ()=>InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test1.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test1.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_S));
 
-        //Test 2
+        //Test 2 - Test for base + laser placement
+        
+        //P1 Place Base
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_E));
+        
+        //P2 Place Base
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_K));
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_K));
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_O));
+
+        //P1 Place Laser
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_E));
         test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_S));
-        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
-        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_W));
-        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_D));
 
+        //P2 Place laser
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_K));
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_K));
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_O));
+        test2.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_I));
+
+        //Test 3 - Test for placing every block
+        test3.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_1));
+        test3.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_2));
+        test3.Add(() => InputSimulator.SimulateKeyPress(VirtualKeyCode.VK_3));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        getTest();
-		
+        getTest();	
 	}
 
 	IEnumerator doTest(List<Action> keyPressList) {
@@ -49,11 +65,14 @@ public class test : MonoBehaviour {
 	}
 
     void getTest() {
-        if (Input.GetKeyDown("1")){
+        if (Input.GetKeyDown("[1]")){
             StartCoroutine(doTest(test1));
         }
-        if (Input.GetKeyDown("2")) {
+        if (Input.GetKeyDown("[2]")) {
             StartCoroutine(doTest(test2));
+        }
+        if (Input.GetKeyDown("[3]")) {
+            StartCoroutine(doTest(test3));
         }
     }
 }
