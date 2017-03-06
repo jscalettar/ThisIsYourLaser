f39b80776f1eb4430cf39dff772f672cbf20c1d0
Franz Breedon <fbreedon@ucsc.edu>
2017-03-05 23:52:31 -0800

    Options and Instructions in Pause Menu
    Implemented the Options and Instructions buttons into the pause menu.
    They show up as windows instead of changing the whole screen, and
    disable the pause menu buttons while active.

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -6,33 +6,69 @@ using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
-
+    // Game objects to set active and deactivate
     public GameObject PauseMenu;
+    public GameObject OptionMenu;
+    public GameObject InstructMenu;
+
+    // Vars for menu buttons
     public Button Pause;
     public Button Resume;
-    /* --- Unimplemented Buttons ---
     public Button Instructions;
     public Button Options;
-       --- Unimplemented Buttons ---*/
     public Button End;
-    public bool isPaused = false;
+
+    // Vars for other buttons
+    public Button Back;
+    public Toggle fullscreenToggle;
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    //Vars for screen options
+    public bool isFull;
+    public Dropdown resDrop;
+    List<string> resos = new List<string>() { "800x600", "1024x768", "1280x720", "1920x1080" };
+
+    //Vars for sounds options
+    public float masterVol;
+    public float musicVol;
+    public float sfxVol;
 
     // Use this for initialization
     void Start()
     {
+        // Set the menus to inactive
         PauseMenu.SetActive(false);
-        Pause = Pause.GetComponent<Button>();
-        //Resume = Resume.GetComponent<Button>();
-        // Instructions = Instructions.GetComponent<Button>();
-        // Options = Options.GetComponent<Button>();
-        //End = End.GetComponent<Button>();
+        OptionMenu.SetActive(false);
+        InstructMenu.SetActive(false);
+
+        // Set pause menu buttons to active
+        Pause.interactable = true;
+        Resume.interactable = true;
+        Instructions.interactable = true;
+        Options.interactable = true;
+        End.interactable = true;
+
+        // Other stuff
+        Screen.fullScreen = false;
+        populateList();
     }
 
+    void Update()
+    {
+        //setMaster(masterSlider.value);
+        //setMusic(musicSlider.value);
+        //setSFX(sfxSlider.value);
+    }
+
+    // -------------------------------------------------------- //
+    // --- <<< --- +++ --- Button Functions --- +++ --- >>> --- //
+    // -------------------------------------------------------- //
     public void pauseGame()
     {
         // Stops Update functions, essentially pausing the game
         Time.timeScale = 0F;
-        isPaused = true;
         PauseMenu.SetActive(true);
     }
 
@@ -40,16 +76,105 @@ public class pauseMenu : MonoBehaviour
     {
         // Sets the game time to realtime
         Time.timeScale = 1F;
-        isPaused = false;
         PauseMenu.SetActive(false);
+    }
+
+    public void optionsMenu()
+    {
+        OptionMenu.SetActive(true);
+        InstructMenu.SetActive(false);
+
+        // Disable pause menu buttons when options are up
+        Pause.interactable = false;
+        Resume.interactable = false;
+        Instructions.interactable = false;
+        Options.interactable = false;
+        End.interactable = false;
+    }
+
+    public void instructionsMenu()
+    {
+        InstructMenu.SetActive(true);
+        OptionMenu.SetActive(false);
+
+        // Disable pause menu buttons when instructions are up
+        Pause.interactable = false;
+        Resume.interactable = false;
+        Instructions.interactable = false;
+        Options.interactable = false;
+        End.interactable = false;
     }
 
     public void endGame()
     {
+        // Resets the time scale so the game doesn't freeze when restarted
         SceneManager.LoadScene("TitleScreen");
         Time.timeScale = 1F;
-        isPaused = false;
         PauseMenu.SetActive(false);
+        OptionMenu.SetActive(false);
+        InstructMenu.SetActive(false);
     }
 
+    public void goBack()
+    {
+        OptionMenu.SetActive(false);
+        InstructMenu.SetActive(false);
+
+        // Set pause menu buttons to active
+        Pause.interactable = true;
+        Resume.interactable = true;
+        Instructions.interactable = true;
+        Options.interactable = true;
+        End.interactable = true;
+    }
+
+    // ------------------------------------------------------- //
+    // --- <<< --- +++ --- Other Functions --- +++ --- >>> --- //
+    // ------------------------------------------------------- //
+    public void setMaster(float vol)
+    {
+        SoundManager.globalVolume = vol;
+        //Need actual sound source to implement
+    }
+
+    public void setMusic(float vol)
+    {
+        SoundManager.globalMusicVolume = vol;
+        //Need actual sound source to implement
+    }
+
+    public void setSFX(float vol)
+    {
+        SoundManager.globalSoundsVolume = vol;
+        //Need actual sound source to implement
+    }
+
+    public void toggleFullscreen()
+    {
+        isFull = Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void changeRes(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                Screen.SetResolution(800, 600, true);
+                break;
+            case 1:
+                Screen.SetResolution(1024, 768, true);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, true);
+                break;
+            case 3:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+        }
+    }
+
+    public void populateList()
+    {
+        resDrop.AddOptions(resos);
+    }
 }
