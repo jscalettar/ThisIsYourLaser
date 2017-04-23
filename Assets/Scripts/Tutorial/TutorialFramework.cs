cd48b426583f425e878b31e1dd41b8c539aca295
nccarlso <nccarlso@ucsc.edu>
2017-04-23 03:48:28 -0700

    Tutorial Framework
    Tutorial Damage Popups and initial creature placement implemented, rest
    is WIP

---
+++ Assets/Scripts/Tutorial/TutorialFramework.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Still need to remove player 2 from tutorial, simplify it etc.
+// Movement event popup triggers not yet implemented
+// Building destruction related popups, and initial building spawns working
+
+public class TutorialFramework : MonoBehaviour {
+
+    public static bool tutorialActive = false;
+    public GameObject Popup;    // Generic game object for displaying popups
+
+    private TutorialModule activeTutorial;
+
+    public void buildingDestructionEvent(XY pos, Building building)
+    {
+        if (activeTutorial.specificDestroyed.Count > 0) {
+            for (int i = 0; i < activeTutorial.specificDestroyed.Count; i++) {
+                if (activeTutorial.specificDestroyed[i] != null && activeTutorial.specificDestroyed[i].pos == pos) {
+                    displayPopup(activeTutorial.specificDestroyed[i].popup, pos.x, pos.y); activeTutorial.specificDestroyed[i] = null; return;
+                }
+            }
+        }
+        if (activeTutorial.baseDestroyed != null && building == Building.Base) {
+            displayPopup(activeTutorial.baseDestroyed); activeTutorial.baseDestroyed = null; return;
+        } else if (activeTutorial.firstDestroyed != null) {
+            displayPopup(activeTutorial.firstDestroyed); activeTutorial.firstDestroyed = null; return;
+        }
+    }
+        
+    private void displayPopup(Sprite tex, int x = -1, int y = -1)
+    {
+        if (x > 0 && y > 0) Popup.transform.localPosition = gridManager.theGrid.coordsToWorld(x, y);
+        else Popup.transform.localPosition = Vector3.zero;
+
+        Popup.GetComponent<SpriteRenderer>().sprite = tex;
+        Popup.GetComponent<SpriteRenderer>().enabled = true;
+
+        Time.timeScale = 0;
+    }
+
+    private void closePopup()
+    {
+        // Add functionality for sequence of popups?
+        Time.timeScale = 1;
+        Popup.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    private void nextTutorialLevel() // WIP
+    {
+        if (activeTutorial.moduleOrderIndex + 1 >= transform.childCount) return;
+        activeTutorial = transform.GetChild(activeTutorial.moduleOrderIndex+1).GetComponent<TutorialModule>();
+        // board reset needs to go here
+        spawnInitialCreatures();
+    }
+
+    private void spawnInitialCreatures()
+    {
+        if (activeTutorial != null && activeTutorial.spawnList != null && activeTutorial.spawnList.Count > 0) {
+            for (int i = 0; i < activeTutorial.spawnList.Count; i++) {
+                TutorialModule.SpawnItem item = activeTutorial.spawnList[i];
+                gridManager.theGrid.placeBuilding(item.x, item.y, item.building, item.player, item.direction, true);
+            }
+        }
+    }
+
+	void Update () {
+        // Check if popup is active, and close it if cancel pressed
+        if (Time.timeScale == 0 && (Input.GetButtonDown("cancel_1") || Input.GetButtonDown("cancel_2"))) closePopup();
+    }
+
+    void Start ()
+    {
+        spawnInitialCreatures();
+    }
+
+    // Toggle Tutorial Game State
+    void OnEnable()
+    {
+        Popup.GetComponent<SpriteRenderer>().enabled = false;
+        activeTutorial = transform.GetChild(0).GetComponent<TutorialModule>();
+        tutorialActive = true;
+    }
+
+    void OnDisable()
+    {
+        tutorialActive = false;
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum tutorialTrigger { placed, moved, removed };
+
+public class TutorialModule : MonoBehaviour {
+    [Tooltip("For keeping track of the order of tutorial levels, used when cycling through levels")]
+    [Header("Module Index")]
+    public int moduleOrderIndex;
+    [Tooltip("List of creatures to place into tutorial level")]
+    [Header("Initial Creature Spawnlist")]
+    public List<SpawnItem> spawnList;
+    [Header("Initial Popup(s)")]
+    public Sprite initialPopup;                              // Popup that appears when you first start the tutorial
+    public Sprite initialPopup2;                             // Another popup that appears when you first start the tutorial (optional)
+    public Sprite initialPopup3;                             // Another popup that appears when you first start the tutorial (optional)
+    [Header("Destruction Popups")]
+    public Sprite firstDestroyed;                            // Popup for first creature that is destroyed, leave it null if none
+    public Sprite baseDestroyed;                             // Popup when base is destroyed
+    public List<specificDestructionPopup> specificDestroyed;    // Popups for when buildings at specific locations are destroyed
+    [Header("Interaction Popups")]
+    public Sprite firstPlacing;      
+    public Sprite firstPlaced;       
+    public Sprite firstMoving;       
+    public Sprite firstMovingPlacing;
+    public Sprite firstMoved; 
+    public Sprite firstRemoving;
+    public Sprite firstRemoved;
+    public List<tutorialTrigger> specificInteraction;           // Popups for when creatures at specific locations are moved/removed/placed
+
+    [Serializable]
+    public class SpawnItem
+    {
+        public int x;
+        public int y;
+        public Building building;
+        public Player player;
+        public Direction direction;
+    }
+
+    [Serializable]
+    public class specificDestructionPopup
+    {
+        public XY pos;
+        public Sprite popup;
+    }
+
+    [Serializable]
+    public class specificInteractionPopup
+    {
+        public XY pos;
+        public tutorialTrigger type;
+        public Sprite popup;
+    }
+}
