3a73dfd769e6a32517faa5d2056834f650f5a284
Niklas C Carlson <nccarlso@crm.ucsc.edu>
2017-05-02 14:40:28 -0700

    .

---
+++ Assets/Scripts/Tutorial/tutorialToInstructionFlag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CreateAssetMenu(menuName = "tutorialToInstructionFlag")]
+public class tutorialToInstructionFlag : ScriptableObject {
+
+    private static tutorialToInstructionFlag Instance;
+    public static tutorialToInstructionFlag instance {
+        get {
+            if (!Instance) Instance = Resources.Load("tutorialToInstructionFlag") as tutorialToInstructionFlag;
+            return Instance;
+        } }
+
+    public bool flag;
+}
