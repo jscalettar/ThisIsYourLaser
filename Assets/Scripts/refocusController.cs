61b75d906b26695c48f6fd1b4789cd39411bf1ea
Andy Espejo <Andy Espejo>
2017-05-24 17:08:46 -0700

    Fixed bug where clicking anywhere with mouse with break controller navigation in menus

---
+++ Assets/Scripts/refocusController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//This fixes a bug that causes controller input to stop working
+public class refocusController : MonoBehaviour {
+
+    GameObject lastSelectedObject;
+
+	void Start() {
+        lastSelectedObject = new GameObject();
+    }
+    
+	void Update () {
+		if(EventSystem.current.currentSelectedGameObject == null) {
+            EventSystem.current.SetSelectedGameObject(lastSelectedObject);
+        } else {
+            lastSelectedObject = EventSystem.current.currentSelectedGameObject;
+        }
+	}
+}
