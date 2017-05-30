b2716b60deaa910d4cb9959cc1a4a5084f9f10f0
Michael Khalil <mkhali1@crm.ucsc.edu>
2017-05-30 15:00:01 -0700

    Fixed controller in menu, also disabled mouse

--- Assets/Scripts/refocusController.cs
+++ Assets/Scripts/refocusController.cs
@@ -15,33 +15,29 @@ public class refocusController : MonoBehaviour {
     public GameObject eventSystem;
     public GameObject eventSystem2;
     public GameObject eventSystem3;
+	public GameObject tempEventSystem;
 
 
-    void Start() {
+	void Start() {
         firstSelectedObject = new GameObject();
         firstSelectedObject = EventSystem.current.currentSelectedGameObject;
         lastSelectedObject = new GameObject();
     }
     
 	void Update () {
-
         if (EventSystem.current.currentSelectedGameObject == null) {
             EventSystem.current.SetSelectedGameObject(lastSelectedObject);
         } else if(lastSelectedObject != null){
             lastSelectedObject = EventSystem.current.currentSelectedGameObject;
         }
-
-        if (EventSystem.current.IsPointerOverGameObject()) {
-            EventSystem.current.SetSelectedGameObject(null);
-        }
+			
         if (Input.GetJoystickNames().Length > 0) {
-            //print(Input.GetJoystickNames());
             Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.lockState = CursorLockMode.Confined;
         }
 
-        if (Input.GetKeyDown("joystick 1 button 7")) { eventSystem3.GetComponent<EventSystem>().enabled = false; eventSystem2.GetComponent<EventSystem>().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = true; }
-        if (Input.GetKeyDown("joystick 2 button 7")) { eventSystem3.GetComponent<EventSystem>().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = false; eventSystem2.GetComponent<EventSystem>().enabled = true; }
-        if (Input.anyKey) { eventSystem2.GetComponent<EventSystem>().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = false; eventSystem3.GetComponent<EventSystem>().enabled = true; }
+		if (Input.GetKeyDown("joystick 1 button 7") || Input.GetAxis("xboxLeftVert") != 0 || Input.GetAxis("xboxLeftHor") != 0) {if (tempEventSystem != null) {tempEventSystem.GetComponent<EventSystem> ().enabled = false;  } eventSystem3.GetComponent<EventSystem>().enabled = false; eventSystem2.GetComponent<EventSystem> ().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = true; }
+		if (Input.GetKeyDown("joystick 2 button 7") || Input.GetAxis("xboxLeftVert2") != 0 || Input.GetAxis("xboxLeftHor2") != 0) { if (tempEventSystem != null) {tempEventSystem.GetComponent<EventSystem> ().enabled = false;  }eventSystem3.GetComponent<EventSystem>().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = false; eventSystem2.GetComponent<EventSystem>().enabled = true; }
+		if (Input.anyKey) {if (tempEventSystem != null) {tempEventSystem.GetComponent<EventSystem> ().enabled = false;  } eventSystem2.GetComponent<EventSystem>().enabled = false; eventSystem.GetComponent<EventSystem>().enabled = false; eventSystem3.GetComponent<EventSystem>().enabled = true; }
     }
 }
